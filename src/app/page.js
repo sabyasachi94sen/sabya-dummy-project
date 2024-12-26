127526d926b0f14a1b053dcb2290ac521121d76e
sabyasachi94sen <sabyasachi9600@gmail.com>
2024-12-26 10:40:18 +0530

    home page added

--- src/app/page.js
+++ src/app/page.js
@@ -2,114 +2,63 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import { MenuOverLay } from "@/components/MenuOverLay";
 
 export default function Home() {
-  const [data, setData] = useState([])
 
-  // useEffect(() => {
-  //   fetch('https://dummyjson.com/c/3029-d29f-4014-9fb4', {
-  //     method: 'GET',
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'
-  //     }
-  //   }).then((d) => d?.json())?.then((info) => {
-  //     setData(info)
-  //   })
-  // }, [])
+  useEffect(() => {
+    if (window != 'undefined') {
+      const hamburgerMenu = document.querySelector("#hamburger-menu");
+      const overlay = document.querySelector("#overlay");
+      const nav1 = document.querySelector("#nav-1");
+      const nav2 = document.querySelector("#nav-2");
+      const nav3 = document.querySelector("#nav-3");
+      const nav4 = document.querySelector("#nav-4");
+      const nav5 = document.querySelector("#nav-5");
+      const navItems = [nav1, nav2, nav3, nav4, nav5];
 
-  console.log(data, 'data')
+      // Control Navigation Animation
+      function navAnimation(val1, val2) {
+        navItems.forEach((nav, i) => {
+          nav.classList.replace(`slide-${val1}-${i + 1}`, `slide-${val2}-${i + 1}`);
+        });
+      }
+
+      function toggleNav() {
+        // Toggle: Hamburger Open/Close
+        hamburgerMenu.classList.toggle("active");
+
+        //   Toggle: Menu Active
+        overlay.classList.toggle("overlay-active");
+
+        if (overlay.classList.contains("overlay-active")) {
+          // Animate In - Overlay
+          overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+
+          // Animate In - Nav Items
+          navAnimation("out", "in");
+        } else {
+          // Animate Out - Overlay
+          overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+
+          // Animate Out - Nav Items
+          navAnimation("in", "out");
+        }
+      }
+
+      // Events Listeners
+      hamburgerMenu.addEventListener("click", toggleNav);
+      navItems.forEach((nav) => {
+        nav.addEventListener("click", toggleNav);
+      });
+    }
+
+  }, [])
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          {/* <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a> */}
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-      <>
-      {/* {data && <div style={{color:'blue'}}>{data?.foo}</div>} */}
-      </>
-    </div>
+    <>
+      <MenuOverLay />
+    </>
   );
 }
---
+++ src/components/MenuOverLay.jsx
@@ -0,0 +1,47 @@
+export const MenuOverLay = () => {
+    return (
+        <>
+            <div className="overlay overlay-slide-left" id="overlay">
+                <nav>
+                    <ul>
+                        <li id="nav-1" className="slide-out-1 center">
+                            <a href="#home" className="center">Home</a>
+                        </li>
+                        <li id="nav-2" className="slide-out-2 center">
+                            <a href="#about" className="center">Products</a>
+                        </li>
+                        <li id="nav-3" className="slide-out-3 center">
+                            <a href="#skills" className="center">Solutions</a>
+                        </li>
+                        <li id="nav-4" className="slide-out-4 center">
+                            <a href="#projects" className="center">Resources</a>
+                        </li>
+                        <li id="nav-5" className="slide-out-5 center">
+                            <a href="#contact" className="center">Company</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="hamburger-menu" id="hamburger-menu">
+                <div className="menu-bar1"></div>
+                <div className="menu-bar2"></div>
+                <div className="menu-bar3"></div>
+            </div>
+            <section id="home" className="center">
+                <p className="logo">Sabyasachi</p>
+            </section>
+            <section id="about" className="center">
+                <h1>Know More Who I am</h1>
+            </section>
+            <section id="skills" className="center">
+                <h1>Here's all the stuff I do</h1>
+            </section>
+            <section id="projects" className="center">
+                <h1>Here's stuff I made recently</h1>
+            </section>
+            <section id="contact" className="center">
+                <h1>Have me make stuff for you</h1>
+            </section>
+        </>
+    )
+}

f53f4a3a726840bd4ec33e5b64b10f4a67863784
sabyasachi94sen <sabyasachi9600@gmail.com>
2024-12-27 09:07:32 +0530

    home layout updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Gemini Chatbot | CodingNepal</title>
+        <title>SLHUB</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
         <link rel="stylesheet" href="style.css" />
       </head>

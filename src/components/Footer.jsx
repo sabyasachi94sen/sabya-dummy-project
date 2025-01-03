1340134e3897c6a9bc8219c994ff981c48340f5b
sabyasachi94sen <sabyasachi9600@gmail.com>
2025-01-03 12:32:44 +0530

    updated ui again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ export const Footer = () => {
                             <div className="text-200 bold footer-title">Platform</div>
                             <div className="footer-list-container">
                                 <ul role="list" className="footer-list-wrapper">
-                                    <li className="footer-list-item"><a href="/connectors" className="footer-link">Connectors</a></li>
-                                    <li className="footer-list-item"><a href="/analytics" className="footer-link">Analytics</a></li>
-                                    <li className="footer-list-item"><a href="/security-and-compliance" className="footer-link">Trust center</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Connectors</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Analytics</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Trust center</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -18,12 +18,12 @@ export const Footer = () => {
                             <div className="text-200 bold footer-title">Product</div>
                             <div className="footer-list-container">
                                 <ul role="list" className="footer-list-wrapper">
-                                    <li className="footer-list-item"><a href="/features/response-generation" className="footer-link">Response generation</a></li>
-                                    <li className="footer-list-item"><a href="/features/enterprise-search" className="footer-link">Enterprise search</a></li>
-                                    <li className="footer-list-item"><a href="/features/personalization" className="footer-link">Personalization</a></li>
-                                    <li className="footer-list-item"><a href="/features/sift-ai" className="footer-link">SiftAI</a></li>
-                                    <li className="footer-list-item"><a href="/features/project-management" className="footer-link">Project management</a></li>
-                                    <li className="footer-list-item"><a href="/features/smart-repository" className="footer-link">Smart repository</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Response generation</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Enterprise search</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Personalization</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">SiftAI</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Project management</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Smart repository</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -31,41 +31,41 @@ export const Footer = () => {
                             <div className="text-200 bold footer-title">Solutions</div>
                             <div className="footer-list-container">
                                 <ul role="list" className="footer-list-wrapper">
-                                    <li className="footer-list-item"><a href="https://www.sifthub.io/solutions/narratives" className="footer-link">Narratives</a></li>
-                                    <li className="footer-list-item"><a href="http://www.sifthub.io/solutions/rfp-rfi" className="footer-link">RFPs &amp; RFIs</a></li>
-                                    <li className="footer-list-item"><a href="http://www.sifthub.io/solutions/security-questionnaire" className="footer-link">Security questionnaires</a></li>
-                                    <li className="footer-list-item"><a href="http://www.sifthub.io/solutions/email-on-call-queries" className="footer-link">Email &amp; On-call queries</a></li>
-                                    <li className="footer-list-item"><a href="http://sifthub.io/solutions/sales-teams" className="footer-link">Sales teams</a></li>
-                                    <li className="footer-list-item"><a href="http://sifthub.io/solutions/presales-teams" className="footer-link">Presales &amp; Solutions teams</a></li>
-                                    <li className="footer-list-item"><a href="http://www.sifthub.io/solutions/bid-proposal-teams" className="footer-link">Bid &amp; Proposal teams</a></li>
-                                    <li className="footer-list-item"><a href="http://www.sifthub.io/solutions/information-security-teams" className="footer-link">Information Security teams</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Narratives</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">RFPs &amp; RFIs</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Security questionnaires</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Email &amp; On-call queries</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Sales teams</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Presales &amp; Solutions teams</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Bid &amp; Proposal teams</a></li>
+                                    <li className="footer-list-item"><a href="#" className="footer-link">Information Security teams</a></li>
                                 </ul>
                             </div>
                         </div>
                         <div id="w-node-_66ef45e3-d717-5fa5-4560-0222e8b9adfc-e8b9adb6" className="footer-links-wrapper">
                             <div className="text-200 bold footer-title">Resources</div>
                             <ul role="list" className="footer-list-wrapper">
-                                <li className="footer-list-item"><a href="/blog" className="footer-link">Blog</a></li>
-                                <li className="footer-list-item"><a href="/podcast" className="footer-link">Podcast</a></li>
+                                <li className="footer-list-item"><a href="#" className="footer-link">Blog</a></li>
+                                <li className="footer-list-item"><a href="#" className="footer-link">Podcast</a></li>
                             </ul>
                         </div>
                         <div id="w-node-_66ef45e3-d717-5fa5-4560-0222e8b9ae06-e8b9adb6" className="footer-links-wrapper">
                             <div className="text-200 bold footer-title">Company</div>
                             <ul role="list" className="footer-list-wrapper">
-                                <li className="footer-list-item"><a href="/about" className="footer-link">About us</a></li>
-                                <li className="footer-list-item"><a href="https://sifthub.hire.trakstar.com/" className="footer-link">Careers</a></li>
-                                <li className="footer-list-item"><a href="/book-a-demo" className="footer-link">Book a demo</a></li>
+                                <li className="footer-list-item"><a href="#" className="footer-link">About us</a></li>
+                                <li className="footer-list-item"><a href="#" className="footer-link">Careers</a></li>
+                                <li className="footer-list-item"><a href="#" className="footer-link">Book a demo</a></li>
                             </ul>
                         </div>
                     </div>
                     <div className="divider-1px margin-2-5rem"></div>
                     <div className="_2-col-wrapper align-center mobile">
                         <div className="div-block-9">
-                            {/* <a href="/" className="footer-logo-wrapper mb-0rem w-inline-block">
+                            {/* <a href="#" className="footer-logo-wrapper mb-0rem w-inline-block">
                             <img src="/img/robot.svg" loading="eager" alt="logo techbit" className="footer-logo sm"  />
                             </a> */}
                             <div className="footer-link">© 2025, SpeedUp Inc.<br /></div>
-                            <a href="/policy/privacy" className="footer-link _w-auto w-inline-block">
+                            <a href="#" className="footer-link _w-auto w-inline-block">
                                 <div className="footer-link">Privacy Policy</div>
                             </a>
                             <a href="#" className="footer-link _w-auto w-inline-block">
